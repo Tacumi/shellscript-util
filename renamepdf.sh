a59441864faf487e84dea3d1b438ff17e5d80a38
Masaaki Horie <horie.masaaki.14@shizuoka.ac.jp>
2018-07-09 15:20:46 +0900

    [fix] renamepdf.sh message error anytime

--- renamepdf.sh
+++ renamepdf.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #
-if [ -x pdfinfo ]
+if [ ! -x /usr/local/bin/pdfinfo ]
 then
-  echo "This script is depends on pdfinfo which is part of xpdf"
+  echo "[error] This script is depends on pdfinfo which is part of xpdf"
+  exit 1
+fi
+if [ ! -e *.pdf ]; then
+  echo "[error] This script renames all PDF files in current direcory \
+with it title of metadata"
+  echo "[error] It's seems to be no PDF files."
+  exit 1
+elif [ ! -w *.pdf ]; then
+  echo "[error] This script renames all PDF files in current direcory \
+with it title of metadata"
+  echo "[error] Permission Denied: Write Error"
+  exit 1
 fi
 for i in *.pdf; do
   pdftitle=$(pdfinfo ${i} | grep Title | cut -d ':' -f 2 | xargs | tr ' ' '_' | tr '/' '_')
